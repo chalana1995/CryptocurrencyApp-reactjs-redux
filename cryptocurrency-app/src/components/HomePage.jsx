3bc84cd9f32641855025f21fca9249e64ffec283
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-09-28 20:24:28 +0530

    home page crypto card part done

--- cryptocurrency-app/src/components/HomePage.jsx
+++ cryptocurrency-app/src/components/HomePage.jsx
@@ -3,11 +3,12 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
 
 const { Title } = Typography;
 
 function HomePage() {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(12);
   const gloabalStats = data?.data?.stats;
 
   if (isFetching) {
@@ -51,6 +52,24 @@ function HomePage() {
           />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
